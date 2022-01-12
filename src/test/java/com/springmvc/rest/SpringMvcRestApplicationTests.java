b371bfc92a5852f991f6c3070401a262fe4157c3
Fatih <curaat79@gmail.com>
2022-01-12 14:39:27 +0300

    project structure

---
+++ src/test/java/com/springmvc/rest/SpringMvcRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvc.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
